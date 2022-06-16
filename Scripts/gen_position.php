50e905b397da12d587e6eb28758e85e8e2f851f0
TheToto318 <th.rx@hotmail.fr>
2022-06-16 11:10:05 +0200

    Add comment to script

--- Scripts/gen_position.php
+++ Scripts/gen_position.php
@@ -3,7 +3,7 @@ function get_distance ($x, $y){
 	include ("mysql.php");
 	$requete = "SELECT distance.C1, distance.C2, distance.C3
 			FROM position 
-			LEFT JOIN distance ON distance.id_position = position.id
+			LEFT JOIN distance ON distance.id_position = position.id 				/*Return an array with the three sensors distance with an x and y coordonate*/
 			WHERE position.x = $x AND position.y = $y;";
 	$resultat = mysqli_query($id_bd, $requete)
 		or die("Execution de la requete impossible : $requete");
@@ -16,7 +16,7 @@ function get_intensity ($d){
 	include ("mysql.php");
 	$requete = "SELECT intensite FROM amplitude WHERE distance = $d;";
 	$resultat = mysqli_query($id_bd, $requete)
-		or die("Execution de la requete impossible : $requete");
+		or die("Execution de la requete impossible : $requete");					/*Return signal intensity of the input distance*/
 	$ligne=mysqli_fetch_assoc($resultat);
 	extract($ligne);
 	return $intensite;
@@ -24,45 +24,45 @@ function get_intensity ($d){
 }
 
 function convert_binary ($i){
-	$i = $i * 100;
+	$i = $i * 100;																	/*Multiply by 100 (to avoid floating number) and convert into binary the signal intensity */
 	return decbin("$i");
 }
 
 function send_to_mqtt ($payload, $capteur){
 	$broker = "127.0.0.1";
-	$topic = "iut/sae24/$capteur";
+	$topic = "iut/sae24/$capteur";													/*Publish a payload to the mqtt broker on a topic including the sensor name*/
 	shell_exec("mosquitto_pub -h $broker -t $topic -m $payload");
 }
 
 
-$x = 0.25;
+$x = 0.25;	/*Initial coordonate */
 $y = 0.25;
-while (($x < 7.75) && ($y < 7.75)){
-	$random = shell_exec("shuf -i 0-1 -n 1");
+while (($x < 7.75) && ($y < 7.75)){				/*Run this loop until the generated coordonates reach the end of the grid */
+	$random = shell_exec("shuf -i 0-1 -n 1");	
 	if ($random == 0) {
-		$x = $x + 0.5;
+		$x = $x + 0.5;							/*Move forward of one cell on the x axis one in two else move on the y axis. Take ramdomly the number 0 or 1 to make the choice*/
 	} else {
 		$y = $y + 0.5;
 	}
-    $dis=get_distance($x, $y);
-    for ($i = 0; $i <= 2; $i++){
-    	$intens=get_intensity($dis[$i]);
-    	$bin=convert_binary($intens);
+    $dis=get_distance($x, $y);					/*Get sensors distances for the generated coordonates*/
+    for ($i = 0; $i <= 2; $i++){				/*Run the loop for the three distances*/
+    	$intens=get_intensity($dis[$i]);		/*Get the intensity of the 'i' distance */
+    	$bin=convert_binary($intens);			/*Convert it to binary*/
     	if ($i == 0) {
     		$cap = "C1";
     	}
-    	elseif ($i == 1) {
+    	elseif ($i == 1) {						/*Set the sensors name according to i*/
     		$cap = "C2";
     	}
     	else {
     		$cap = "C3";
     	}
-    	send_to_mqtt($bin, $cap);
-    	printf("FSK %d : %d \n", $i+1, $bin);
+    	send_to_mqtt($bin, $cap);			/*Send the payload to the MQTT broker with the sensor name */
+    	printf("FSK %d : %d \n", $i+1, $bin); /*Logging*/ 
 
     }
     printf("x : %f, y : %f\n",$x, $y);
     printf("----------------------------------------------\n");
-    sleep(5);
+    sleep(5); /*Script standby for 5 sec*/
 }
 ?>
--- Scripts/recover_position.php
+++ Scripts/recover_position.php
@@ -1,6 +1,6 @@
 <?php
 function decode_binary ($i){
-	$i=bindec("$i");
+	$i=bindec("$i");			/*Decode the binary payload and devide it by 100 (see gen_position.php)*/
 	$i = $i / 100;
 	return $i;
 }
@@ -9,7 +9,7 @@ function get_distance ($d){
 	include ("mysql.php");
 	$requete = "SELECT distance FROM amplitude WHERE intensite = $d;";
 	$resultat = mysqli_query($id_bd, $requete)
-		or die("Execution de la requete impossible : $requete");
+		or die("Execution de la requete impossible : $requete");			/*Get a distance with an intensity payload */
 	$ligne = mysqli_fetch_array($resultat);
 	extract($ligne);
 	return $distance;
@@ -20,7 +20,7 @@ function get_coordonate ($d1, $d2, $d3){
 	include ("mysql.php");
 	$requete = "SELECT position.*, distance.C1, distance.C2, distance.C3 FROM position 
 	LEFT JOIN distance ON distance.id_position = position.id
-	WHERE distance.C1 = $d1 AND distance.C2 = $d2 AND distance.C3 = $d3;";
+	WHERE distance.C1 = $d1 AND distance.C2 = $d2 AND distance.C3 = $d3;";					/*Get x and y coordonates with the three sensors distances*/
 	$resultat = mysqli_query($id_bd, $requete)
 		or die("Execution de la requete impossible : $requete");
 	$ligne = mysqli_fetch_array($resultat);
@@ -36,7 +36,7 @@ function insert_measure ($payload){
 	$requete = "SELECT position.id FROM position
 				WHERE position.x = $x AND position.y = $y;";
 	$resultat = mysqli_query($id_bd, $requete)
-		or die("Execution de la requete impossible : $requete");
+		or die("Execution de la requete impossible : $requete");	/*Send measurement on the 'mesure' table from a array payload including the x and y coordonates by parsing the matching id on the position table*/
 	$ligne = mysqli_fetch_array($resultat);
 	extract($ligne);			
 	$date = date("Y-m-d");
@@ -46,22 +46,22 @@ function insert_measure ($payload){
 			or die("Execution de la requete impossible : $requete");
 }
 
-while (true) {
-	$str=shell_exec("mosquitto_sub -t iut/# -v -C 3");
-	$array = preg_split("/\r\n|\n|\r/", $str);
+while (true) {												/*Run this loop until the script is stopped*/
+	$str=shell_exec("mosquitto_sub -t iut/# -v -C 3");		/*Subscribe all the topics starting by 'iut' and output only three lines with the payload and topic*/
+	$array = preg_split("/\r\n|\n|\r/", $str);				/*Create an array by separating the output by using line return of separators. */ 
 	/*print_r($array);*/
-	$space=' ';
+	$space=' ';												/*Set separators variable*/
 	$slash='/';
-	$tp_cap=array();
+	$tp_cap=array();										/*Create two empty arrays.*/
 	$distances=array();
-	for ($i=0; $i < 3; $i++) { 
-		array_push($tp_cap,explode($space, $array[$i]));
-		$tp_cap[$i][0]=explode($slash, $tp_cap[$i][0]);
-		$tp_cap[$i][1]=decode_binary($tp_cap[$i][1]);
+	for ($i=0; $i < 3; $i++) { 								/*Run this loop for each generated array before*/
+		array_push($tp_cap,explode($space, $array[$i]));	/*Add into the 'tp_cap' array an other array composed of the topic and payload rcovered from the MQTT broker.*/
+		$tp_cap[$i][0]=explode($slash, $tp_cap[$i][0]);		/*Slice the array line that include the topic into an other array with three lines (iut, sae24, 'sensors name') using slash as separator */
+		$tp_cap[$i][1]=decode_binary($tp_cap[$i][1]);		/*Transform the binary intensity into decimal numnber into the array*/ 
 	}
-	$distances=array("dc1"=>get_distance($tp_cap[0][1]), "dc2"=>get_distance($tp_cap[1][1]), "dc3"=>get_distance($tp_cap[2][1]));
-	$coord = get_coordonate($distances["dc1"], $distances["dc2"], $distances["dc3"]);
-	insert_measure($coord);
+	$distances=array("dc1"=>get_distance($tp_cap[0][1]), "dc2"=>get_distance($tp_cap[1][1]), "dc3"=>get_distance($tp_cap[2][1])); /*Create an associative array which include all the three sensors distances for the coordonates*/
+	$coord = get_coordonate($distances["dc1"], $distances["dc2"], $distances["dc3"]); /*Get the coordonates for theses distances*/
+	insert_measure($coord); /*Insert these coordonates into the database*/
 	printf("x : %s, y: %s\n", $coord["x"], $coord["y"]);
 }
 
