993f586827fda8677aff0c02663c1ab8415d751d
TheToto318 <th.rx@hotmail.fr>
2022-06-21 15:03:25 +0200

    Reset auto increment on initial DB

--- Docker/mosquitto/gen_position.php
+++ Docker/mosquitto/gen_position.php
@@ -34,11 +34,58 @@ function send_to_mqtt ($payload, $capteur){
 	shell_exec("mosquitto_pub -h $broker -t $topic -m $payload");
 }
 
+
+function get_sensor_mode () {
+		include ("mysql.php");
+		$requete = "SELECT mode.nbr_capteur FROM mode;";
+		$resultat = mysqli_query($id_bd, $requete)
+			or die("Execution de la requete impossible : $requete");	
+		$ligne=mysqli_fetch_assoc($resultat);
+		extract($ligne);
+		mysqli_close($id_bd);
+		if($nbr_capteur == 1){
+	    		$random = shell_exec("shuf -i 0-2 -n 1");
+	    		echo "Switching to one sensor mode\n";
+	    		echo "-------------------------------\n";	
+				if ($random == 0) {
+					$t1 = 0;
+	    			$t2 = 0;							
+				} elseif ($random == 1) {
+					$t1 = 1;
+	    			$t2 = 1;
+				} else {
+					$t1 = 2;
+	    			$t2 = 2;	
+				}
+			}
+			elseif($nbr_capteur == 2){
+				$random = shell_exec("shuf -i 0-2 -n 1");
+				echo "Switching to two sensors mode\n";
+				echo "-------------------------------\n";	
+				if ($random == 0) {
+					$t1 = 0;
+	    			$t2 = 1;							
+				} elseif ($random == 1) {
+					$t1 = 0;
+	    			$t2 = 2;
+				} else {
+					$t1 = 1;
+	    			$t2 = 2;	
+				}
+			}
+			elseif($nbr_capteur == 3){
+				echo "Switching to three sensors mode\n";
+				echo "-------------------------------\n";
+				$t1 = 0;
+				$t2 = 2;
+			}
+		$result=array("t1"=>$t1, "t2"=>$t2);
+		return $result;
+}
+
 while (true){
 	$x = 0.25;	/*Initial coordonate */
 	$y = 0.25;
-	$t1 = 0;
-	$t2 = 2;
 	while (($x < 7.75) && ($y < 7.75)){				/*Run this loop until the generated coordonates reach the end of the grid */
 		$random = shell_exec("shuf -i 0-1 -n 1");	
 		if ($random == 0) {
@@ -46,8 +93,9 @@ while (true){
 		} else {
 			$y = $y + 0.5;
 		}
-	    $dis=get_distance($x, $y);					/*Get sensors distances for the generated coordonates*/
-	    for ($i = $t1; $i <= $t2; $i++){				/*Run the loop for the three distances*/
+	    $dis=get_distance($x, $y);
+	    $t1_t2=get_sensor_mode();	                                          /*Get sensors distances for the generated coordonates*/
+	    for ($i = $t1_t2["t1"]; $i <= $t1_t2["t2"]; $i++){				/*Run the loop for the three distances*/
 	    	$intens=get_intensity($dis[$i]);		/*Get the intensity of the 'i' distance */
 	    	$bin=convert_binary($intens);			/*Convert it to binary*/
 	    	if ($i == 0) {
@@ -65,66 +113,8 @@ while (true){
 	    }
 	    printf("x : %f, y : %f\n",$x, $y);
 	    printf("----------------------------------------------\n");
-	    echo "Switch mode ? (5sec) 1 : One sensor, 2 : Two sensors, 3 : Three sensors\n";
+	sleep(10);
 
-		// get file descriptor for stdin 
-		$fd = fopen('php://stdin', 'r');
-
-		// prepare arguments for stream_select()
-		$read = array($fd);
-		$write = $except = array(); // we don't care about this
-		$timeout = 5;
-
-		// wait for maximal 5 seconds for input
-		if(stream_select($read, $write, $except, $timeout)) {
-	    	$menuchoice=fgets($fd);
-	    	if($menuchoice == 1){
-	    		$random = shell_exec("shuf -i 0-2 -n 1");
-	    		echo "Switching to one sensor mode\n";
-	    		echo "-------------------------------\n";	
-				if ($random == 0) {
-					echo "ok 1\n";
-					$t1 = 0;
-	    			$t2 = 0;							
-				} elseif ($random == 1) {
-					$t1 = 1;
-	    			$t2 = 1;
-				} else {
-					$t1 = 2;
-	    			$t2 = 2;	
-				}
-			}
-			elseif($menuchoice == 2){
-				$random = shell_exec("shuf -i 0-2 -n 1");
-				echo "Switching to two sensors mode\n";
-				echo "-------------------------------\n";	
-				if ($random == 0) {
-					$t1 = 0;
-	    			$t2 = 1;							
-				} elseif ($random == 1) {
-					$t1 = 0;
-	    			$t2 = 2;
-				} else {
-					$t1 = 1;
-	    			$t2 = 2;	
-				}
-			}
-			elseif($menuchoice == 3){
-				echo "Switching to three sensors mode\n";
-				echo "-------------------------------\n";
-				$t1 = 0;
-				$t2 = 2;
-			} 
-			else {
-				echo "Please input a number ! \n";
-				echo "------------------------\n";
-			}
-		}
-		else {
-	    	echo "Timeout\n";
-	    	echo "-----------------\n";
-			}
-		sleep(10);
-	}
+}
 }
 ?>
